1e50aa18f05a5176c169b575bd8e059a3c66fa44
Ninke Dijkstra <ninke.dijkstra@businessdecision.nl>
2023-01-31 14:07:52 +0000

    parameterized flow

---
+++ week_2/flows/homework/github_deploy.py
@@ -0,0 +1,14 @@
+from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+from parameterized_flow import etl_parent_flow
+
+github_block = GitHub.load("github-block")
+
+github_dep = Deployment.build_from_flow(
+    flow=etl_parent_flow,
+    name="github-flow",
+    storage = github_block
+    )
+
+if __name__ == "__main__":
+    github_dep.apply()
