cd6c4cad98591685d24bb951ba5df5978e515fcd
Ninke Dijkstra <ninke.dijkstra@businessdecision.nl>
2023-01-31 14:27:18 +0000

    tpep to lpep

--- week_2/flows/homework/github_deploy.py
+++ week_2/flows/homework/github_deploy.py
@@ -2,12 +2,15 @@ from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
 from parameterized_flow import etl_parent_flow
 
+
 github_block = GitHub.load("github-block")
+
 
 github_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
     name="github-flow",
-    storage = github_block
+    storage = github_block,
+    path = "week_2/flows/homework"
     )
 
 if __name__ == "__main__":
