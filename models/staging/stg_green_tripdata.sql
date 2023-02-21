f8601a4e5991f37caf135297213079ef028c0a61
ninke.dijkstra@businessdecision.nl <ninke.dijkstra@businessdecision.nl>
2023-02-21 12:20:49 +0000

    finish staging and core

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,6 +1,13 @@
 {{ config(materialized='view') }}
 
-select 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by cast(vendorid as integer), lpep_pickup_datetime) as rn
+  from {{ source('staging','external_green_tripdata') }}
+  where vendorid is not null 
+)
+select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
@@ -28,16 +35,15 @@ select
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
-    {{ get_payment_type_description('payment_type') }} as payment_type_description,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
+from tripdata
+where rn = 1
 
-from {{ source('staging', 'external_green_tripdata') }}
 
-where vendorid is not null
-
---dbt build --m <model.sql> --var 'is_test_run:false'
+-- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
-    limit 100
+  limit 100
 
 {% endif %}
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -1,7 +1,14 @@
 {{ config(materialized='view') }}
-
-select 
-    -- identifiers
+ 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by cast(vendorid as integer), tpep_pickup_datetime) as rn
+  from {{ source('staging','external_yellow_tripdata') }}
+  where vendorid is not null 
+)
+select
+   -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
@@ -18,7 +25,7 @@ select
     cast(trip_distance as numeric) as trip_distance,
     -- yellow cabs are always street-hail
     1 as trip_type,
-
+    
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
     cast(extra as numeric) as extra,
@@ -31,14 +38,12 @@ select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
-    
-from {{ source('staging', 'external_yellow_tripdata') }}
+from tripdata
+where rn = 1
 
-where vendorid is not null
-
---dbt build --m <model.sql> --var 'is_test_run:false'
+-- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
-    limit 100
+  limit 100
 
 {% endif %}
